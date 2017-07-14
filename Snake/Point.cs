772576c76e58a8a3ae76a3af4b9d6b6c8186c762
rnrru <yershove@gmail.com>
2017-07-14 21:02:44 +0300

    les4

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -12,6 +12,12 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,16 +14,16 @@ namespace Snake
             //int y1 = 3;
             //char symbol1 = '*';
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1,3,'*');
+            //p1.x = 1;
+            //p1.y = 3;
+            //p1.sym = '*';
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4,5,'#');
+            //p2.x = 4;
+            //p2.y = 5;
+            //p2.sym = '#';
 
             p2.Draw();
 
